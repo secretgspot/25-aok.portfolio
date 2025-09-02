3b320d5d6a75be8b07c786ce15ae7d129a7339eb
Secret G Spot <secregspot@gmail.com>
2025-09-01 22:33:30 -0600

    Project cleaned up

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -5,26 +5,24 @@
 <fieldset class="project-card">
 	<legend class="project-title">{title}</legend>
 
-	<header class="project-header">
-		<div class="summary-url">
-			{#if summary}
-				<p class="project-summary">{summary}</p>
-			{/if}
-
-			<a href="/{slug}" class="project-link">View Details</a>
-		</div>
-
-		{#if screenshots && screenshots.length > 0}
-			<div class="project-screenshot-container">
-				<img
-					src={screenshots[0].url}
-					alt="{title} screenshot"
-					class="project-screenshot" />
+	<a href="/{slug}" class="project-header-link">
+		<header class="project-header">
+			<div class="summary-url">
+				{#if summary}
+					<p class="project-summary">{summary}</p>
+				{/if}
 			</div>
-		{/if}
-	</header>
 
-	
+			{#if screenshots && screenshots.length > 0}
+				<div class="project-screenshot-container">
+					<img
+						src={screenshots[0].url}
+						alt="{title} screenshot"
+						class="project-screenshot" />
+				</div>
+			{/if}
+		</header>
+	</a>
 
 	{#if details}
 		<footer class="project-details">
@@ -55,12 +53,76 @@
 		margin: 0;
 		padding: 0;
 
+		.project-title {
+			font-size: var(--size-fluid-2);
+			margin-inline-start: var(--size-9);
+			padding-inline: var(--size-3);
+		}
+
+		.project-header-link {
+			position: relative;
+			display: block;
+			text-decoration: none;
+			color: inherit;
+			&::after {
+				content: 'View Details';
+				position: absolute;
+				top: var(--size-3);
+				right: var(--size-3);
+				color: var(--text-2); /* Default color */
+				font-weight: bold;
+				white-space: nowrap;
+				z-index: 3; /* Above other content */
+				transition: color 0.2s ease-in-out;
+			}
+			&:hover::after {
+				color: var(--text-1); /* Change color on hover */
+			}
+		}
+
 		.project-header {
 			display: grid;
 			position: relative;
 			margin-block-start: calc(var(--size-5) * -1);
+			&::before {
+				content: '';
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100%; /* Cover the entire element */
+				background: linear-gradient(to top, var(--surface-1), transparent 30%);
+				pointer-events: none; /* Allow clicks to pass through */
+			}
+
 			* {
 				grid-area: 1/1;
+			}
+
+			.project-screenshot-container {
+				width: 100%;
+				height: 100%;
+				overflow: hidden;
+				border-radius: var(--radius-4);
+				filter: blur(3px) opacity(0.3);
+				z-index: 1;
+				&::before {
+					content: '';
+					position: absolute;
+					top: 0;
+					left: 0;
+					width: 100%;
+					height: 100%; /* Cover the entire element */
+					background: linear-gradient(to top, var(--surface-1), transparent 50%);
+					pointer-events: none; /* Allow clicks to pass through */
+				}
+
+				.project-screenshot {
+					width: 100%;
+					height: 100%;
+					object-fit: cover; /* Ensures the image covers the area without distortion */
+					aspect-ratio: 3 / 1;
+				}
 			}
 
 			.summary-url {
@@ -74,80 +136,27 @@
 					font-size: var(--size-fluid-1);
 					margin-inline: var(--size-3);
 				}
-
-				.project-link {
-					display: inline-block;
-					color: var(--text-2);
-					text-decoration: none;
-					font-weight: bold;
-					justify-self: end;
-					margin: var(--size-3);
-					white-space: nowrap;
-					&:hover {
-						text-decoration: underline;
-						color: var(--text-1);
-					}
-				}
 			}
 		}
-	}
 
-	.project-title {
-		font-size: var(--size-fluid-2);
-		margin-inline-start: var(--size-9);
-		padding-inline: var(--size-3);
-	}
-
-	.project-screenshot-container {
-		width: 100%;
-		height: 100%;
-		overflow: hidden;
-		border-radius: var(--radius-4);
-		filter: blur(3px) opacity(0.3);
-		z-index: 1;
-		&::before {
-			content: '';
-			position: absolute;
-			top: 0;
-			left: 0;
-			width: 100%;
-			height: 100%; /* Cover the entire element */
-			background: linear-gradient(to top, var(--surface-1), transparent 50%);
-			pointer-events: none; /* Allow clicks to pass through */
+		.project-details {
+			background: var(--surface-3);
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+			padding: var(--size-6);
+			border-bottom-left-radius: calc(var(--radius-4) / 1.2);
+			border-bottom-right-radius: calc(var(--radius-4) / 1.2);
+			position: relative; /* Needed for pseudo-element positioning */
+			&::before {
+				content: '';
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100%; /* Cover the entire element */
+				background: linear-gradient(to bottom, var(--surface-1), transparent 30%);
+				pointer-events: none; /* Allow clicks to pass through */
+			}
 		}
-	}
-
-	.project-screenshot {
-		width: 100%;
-		height: 100%;
-		object-fit: cover; /* Ensures the image covers the area without distortion */
-		aspect-ratio: 3 / 1;
-	}
-
-	/* .project-header:hover .project-screenshot {
-		transform: scale(1.05);
-	} */
-
-	
-
-	.project-details {
-		background: var(--surface-3);
-		display: grid;
-		grid-template-columns: 1fr 1fr;
-		padding: var(--size-6);
-		border-bottom-left-radius: calc(var(--radius-4) / 1.2);
-		border-bottom-right-radius: calc(var(--radius-4) / 1.2);
-		position: relative; /* Needed for pseudo-element positioning */
-	}
-
-	.project-details::before {
-		content: '';
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%; /* Cover the entire element */
-		background: linear-gradient(to bottom, var(--surface-1), transparent 30%);
-		pointer-events: none; /* Allow clicks to pass through */
 	}
 </style>
