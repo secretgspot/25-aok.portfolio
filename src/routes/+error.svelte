f3459a85fca2813881e99c9fa00011797cbc9ba0
Secret G Spot <secregspot@gmail.com>
2025-09-03 23:53:57 -0600

    error page complete

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,16 +1,56 @@
 <script>
 	import { page } from '$app/state';
 	import Logo from '$lib/Logo.svelte';
+	import { onMount } from 'svelte';
+
+	let { position } = $props();
+
+	let logoSrc = $state('');
+
+	function newLogo() {
+		// This might result in a 404 error if a logo for the generated number doesn't exist.
+		const randomNumber = Math.floor(Math.random() * 136) + 1;
+		logoSrc = `/logos/logo_${randomNumber}.webp`;
+	}
+
+	onMount(() => {
+		newLogo();
+	});
 </script>
 
-<h1>{page.status}: {page.error?.message}</h1>
+<article class="error-page">
+	<h1 class="title">{page.status}: {page.error?.message}</h1>
 
-<figure>
-	<img src="random_logo" alt="" />
-	<figcaption>
-		{page.status}
-		{page.url}
-	</figcaption>
-</figure>
+	{#if logoSrc}
+		<img src={logoSrc} alt="Random Logo" class="logo-image" />
+	{/if}
+	<div class="url">{page.url}</div>
+</article>
 
 <Logo position="start" />
+
+<style>
+	.error-page {
+		display: grid;
+		place-content: center;
+		justify-items: center;
+		align-items: center;
+		flex: 1;
+		& > * {
+			grid-area: 1/1;
+		}
+
+		.title {
+			z-index: 1;
+		}
+
+		.logo-image {
+			max-width: 600px;
+			width: 100%;
+			aspect-ratio: 1;
+		}
+		.url {
+			align-self: self-end;
+		}
+	}
+</style>
